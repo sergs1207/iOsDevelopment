571283f7a170ba378c3276cb525800191cee69a3
Sergii Maksiuta <sergiimaksiuta@sergiis-mbp.d-net.kiev.ua>
2017-01-02 16:38:27 +0200

    Added files during first day of study

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLogo: UIImageView!
+    @IBOutlet weak var backgroundAppearance: UIImageView!
+    
+    @IBOutlet weak var helloDudeBtn: UIButton!
+    @IBOutlet weak var goodByeBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+        
+    @IBAction func helloDudeBtnPressed(_ sender: Any) {
+        myLogo.isHidden=false
+        backgroundAppearance.isHidden=false
+        
+        helloDudeBtn.isHidden=true
+        
+        
+    }
+    @IBAction func goodByeBtnPressed(_ sender: Any) {
+        myLogo.isHidden=false
+        backgroundAppearance.isHidden=false
+        
+        goodByeBtn.isHidden=true
+    }
+    
 
 }
 
